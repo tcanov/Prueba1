654ee4dfe74c36b05282f49e9ef8fdc3125dee0e
Tulio Cano <tcanov@gmail.com>
2021-04-10 17:56:49 -0500

    mi primer commit

---
+++ pru.js
@@ -0,0 +1,7 @@
+console.log("nueva forma de trabajar");
+// funcion en typescrip
+function conversionmonetaria(cantidad) {
+    return cantidad * 3700;
+}
+var pesos = conversionmonetaria(100);
+console.log("La cantidad en pesos es : " + pesos);
---
+++ pru.ts
@@ -0,0 +1,10 @@
+console.log("nueva forma de trabajar");
+// funcion en typescrip
+function conversionmonetaria(cantidad:number): number {
+    return cantidad * 3700;
+
+    
+}
+
+var pesos = conversionmonetaria(100);
+console.log("La cantidad en pesos es : " + pesos);
